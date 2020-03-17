7f0b021ff309a34ba04b89393b29c431c311f4ea
Aaron Castro <aaroncrcr@gmail.com>
2020-03-17 10:17:57 -0300

    all templates included

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { IndexComponent } from './pages/index/index.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { BuscadorComponent } from './pages/buscador/buscador.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'buscador', component: BuscadorComponent },
+  { path: 'detalle', component: DetalleComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'index', component: IndexComponent },
+  { path: '404', component: Page404Component },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: '404', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { IndexComponent } from './pages/index/index.component';
+import { BuscadorComponent } from './pages/buscador/buscador.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page404Component,
+    IndexComponent,
+    BuscadorComponent,
+    DetalleComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
